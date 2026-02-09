b9310acac7565e27ceb19447a653416952f0648f
James Arnott <james@rno.tt>
2026-02-09 20:29:56 +0000

    chore: misc update

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,8 +1,10 @@
-import { useSyncExternalStore } from "react";
+import { useLayoutEffect, useRef, useSyncExternalStore } from "react";
 
 import App from "~/App";
 import { ReportPage } from "~/components/report-page";
 import { useExtensionDatabase } from "~/hooks/use-extension-database";
+
+const REPORT_RE = /^#\/report\/([a-p]{32})$/;
 
 function getHash() {
   return window.location.hash;
@@ -20,16 +22,34 @@ function useHash() {
 export function Router() {
   const hash = useHash();
   const { reports } = useExtensionDatabase();
+  const scrollYRef = useRef(0);
+  const prevExtIdRef = useRef<string | undefined>(undefined);
 
-  const reportMatch = hash.match(/^#\/report\/([a-p]{32})$/);
-  if (reportMatch?.[1]) {
-    return (
-      <ReportPage
-        extensionId={reportMatch[1]}
-        ext={reports.get(reportMatch[1])}
-      />
-    );
+  const reportMatch = hash.match(REPORT_RE);
+  const extensionId = reportMatch?.[1];
+
+  // Capture scroll position during render (before DOM mutations)
+  if (extensionId && !prevExtIdRef.current) {
+    scrollYRef.current = window.scrollY;
   }
+  prevExtIdRef.current = extensionId;
 
-  return <App reports={reports} />;
+  // Scroll after DOM commits but before browser paints
+  useLayoutEffect(() => {
+    window.scrollTo(0, extensionId ? 0 : scrollYRef.current);
+  }, [extensionId]);
+
+  return (
+    <>
+      <div hidden={!!extensionId}>
+        <App reports={reports} />
+      </div>
+      {extensionId && (
+        <ReportPage
+          extensionId={extensionId}
+          ext={reports.get(extensionId)}
+        />
+      )}
+    </>
+  );
 }
